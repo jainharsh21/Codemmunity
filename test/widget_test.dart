446213ea3827ff2605b102a0bee7d5840a27f4dd
HARSH JAIN <jainharsh270@gmail.com>
2019-12-14 22:11:59 +0530

    initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:codemmunity/main.dart';
 
 void main() {
+  // probably not needed.
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
